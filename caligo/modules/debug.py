a52237824a9f192eed7abdaba53a767585129900
n/a <n/a>
2025-08-26 03:11:11 +0700

    bdjdj

--- caligo/modules/debug.py
+++ caligo/modules/debug.py
@@ -83,8 +83,7 @@ class Debug(module.Module):
                             i
                             for i in exception.stack
                             if not any(
-                                path in i.filename
-                                for path in ["<string>", "/usr/local/lib/"]
+                                path in i.filename for path in ["/caligo/", "/lib/"]
                             )
                         ]
                     )
